d46c0782d7778dd61d4c717a0ce6123b1f3d9d11
Matteo Tadiello <tadistyle@gmail.com>
2021-10-18 09:36:34 +0200

    Initial commit

---
+++ plot.py
@@ -0,0 +1,23 @@
+import pylab
+from datetime import datetime
+
+def plot_data(self, episodes, scores, max_q_mean, title):
+        # datetime object containing current date and time
+        now = datetime.now()
+         
+        print("now =", now)
+        
+        # dd/mm/YY H:M:S
+        dt_string = now.strftime("%d_%m_%Y__%H_%M_%S_")
+        print("date and time =", dt_string)	
+        pylab.figure(0)
+        pylab.plot(episodes, max_q_mean, 'b')
+        pylab.xlabel("Episodes")
+        pylab.ylabel("Average Q Value")
+        pylab.savefig("{}_{}qvalues_{}.png".format(title,dt_string,self.id_nr))
+
+        pylab.figure(1)
+        pylab.plot(episodes, scores, 'b',linestyle="",marker="o")
+        pylab.xlabel("Episodes")
+        pylab.ylabel("Score")
+        pylab.savefig("{}_{}scores_{}.png".format(title,dt_string,self.id_nr))
